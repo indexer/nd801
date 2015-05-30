9a573c9e66529cc57ab1d344b99cfc8ddcfec3a2
indexer <indexer@seesarcho.(none)>
2015-05-30 13:02:08 +0630

    first commit on nd801

---
+++ app/src/main/java/indexer/com/portfolio/MainActivity.java
@@ -0,0 +1,44 @@
+package indexer.com.portfolio;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Button capstone = (Button) findViewById(R.id.capstone);
+    capstone.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        Toast.makeText(getApplicationContext(), "This button will launch my capstone app!",
+            Toast.LENGTH_LONG).show();
+      }
+    });
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+}
